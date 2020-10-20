602fd618b1b5dd40f3e8cfe4d03ede27e3fffc34
EvgMalikova <evgenimalikova@gmail.com>
2020-10-20 14:39:50 +0100

    old

--- get_mpich.sh
+++ get_mpich.sh
@@ -1,18 +1,24 @@
-#!/bin/sh
-
 if [ -f mpich/lib/libmpich.so ]; then
   echo "libmpich.so found -- nothing to build."
 else
   echo "Downloading mpich source."
-  wget http://www.mpich.org/static/downloads/3.4a3/mpich-3.4a3.tar.gz
-  tar xfz mpich-3.4a3.tar.gz
-  rm mpich-3.4a3.tar.gz
+  wget http://www.mpich.org/static/downloads/3.2/mpich-3.2.tar.gz
+  tar xfz mpich-3.2.tar.gz
+  rm mpich-3.2.tar.gz
   echo "configuring and building mpich."
-  mkdir mpich
-  cd mpich-3.4a3
-  ./configure 
-  make -j2
-  make 
+  cd mpich-3.2
+  ./configure \
+          --prefix=`pwd`/../mpich \
+          --enable-static=false \
+          --enable-alloca=true \
+          --disable-long-double \
+          --enable-threads=single \
+          --enable-fortran=no \
+          --enable-fast=all \
+          --enable-g=none \
+          --enable-timing=none
+  make -j4
+  make install
   cd -
+  rm -rf mpich-3.2
 fi
-
